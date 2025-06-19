887450835b8cbcf98f07825e056ae7ec0ec4d822
Randy Eckman <emanspeaks@gmail.com>
2025-06-18 19:18:38 -0500

    init commit

---
+++ build-release-stage3.bat
@@ -0,0 +1,104 @@
+setlocal
+:: check these versions before running!
+set DEVKIT_VERSION=0.15.0-dev.233+7c85dc460
+set NINJA_VERSION=1.13.0
+set CMAKE_VERSION=4.0.3
+rem set ZIG_COMMIT=master
+set ZIG_COMMIT=ef35c3d5fefb8c14e17f3c7036bb21e808ee59be
+set ZIG_CMAKE_FLAGS=-DCMAKE_BUILD_TYPE=Release -DZIG_NO_LIB=ON
+set ZIGROOT=C:/zig
+
+:: precompute paths and file names
+set DEVKIT_LONGNAME=zig+llvm+lld+clang-x86_64-windows-gnu-%DEVKIT_VERSION%
+set DEVKIT_NAME=devkit-%DEVKIT_VERSION%
+set CMAKE_LONGNAME=cmake-%CMAKE_VERSION%-windows-x86_64
+set CMAKE_NAME=cmake-%CMAKE_VERSION%
+
+:: Make sure to use forward slashes (/) for all path separators
+:: (otherwise CMake will try to interpret backslashes as escapes and fail).
+:: We will assume all paths are POSIX except those ending in `_WIN`.
+set "ZIGROOT=%ZIGROOT:\=/%"
+set "ZIGROOT_WIN=%ZIGROOT:/=\%"
+
+set ZIGROOTBIN=%ZIGROOT%/bin
+set "ZIGROOTBIN_WIN=%ZIGROOTBIN:/=\%"
+
+set DOWNLOADS=%ZIGROOT%/downloads
+set "DOWNLOADS_WIN=%DOWNLOADS:/=\%"
+
+set DEVKIT=%ZIGROOT%/%DEVKIT_NAME%
+set "DEVKIT_WIN=%DEVKIT:/=\%"
+
+set ZIG_SRC=%ZIGROOT%/zig-src
+set "ZIG_SRC_WIN=%ZIG_SRC:/=\%"
+
+set ZIG_BUILD=%ZIG_SRC%/build
+set "ZIG_BUILD_WIN=%ZIG_BUILD:/=\%"
+
+set ZIG_EXE=%DEVKIT%/bin/zig.exe
+set "ZIG_EXE_WIN=%ZIG_EXE:/=\%"
+
+set NINJA_EXE=%ZIGROOTBIN%/ninja.exe
+set "NINJA_EXE_WIN=%NINJA_EXE:/=\%"
+
+set CMAKE_EXE=%ZIGROOTBIN%/%CMAKE_NAME%/bin/cmake.exe
+set "CMAKE_EXE_WIN=%CMAKE_EXE:/=\%"
+
+mkdir %DOWNLOADS_WIN%
+mkdir %ZIGROOTBIN_WIN%
+
+:: set PATH to a very minimal set of values to limit bad dependency resolution
+:: I think something in my path on work laptop is polluting dependencies,
+:: probably zlib since that has caused problems for me in the past on this PC
+set WINSYS32=%SystemRoot%\System32
+set PATH=%ZIGROOTBIN_WIN%;%ZIGROOTBIN_WIN%\%CMAKE_NAME%\bin;%WINSYS32%;%SystemRoot%;%WINSYS32%\Wbem;%WINSYS32%\WindowsPowerShell\v1.0\;%WINSYS32%\OpenSSH\;%ProgramFiles%\dotnet\;%LOCALAPPDATA%\Microsoft\WindowsApps;%LOCALAPPDATA%\Programs\Git\bin
+
+set DEVKIT_VER_TMP=
+if exist %ZIG_EXE_WIN% (for /F "tokens=*" %%g in ('%ZIG_EXE_WIN% version') do (set DEVKIT_VER_TMP=%%g))
+if not "%DEVKIT_VER_TMP%" == "%DEVKIT_VERSION%" (
+  curl -o %DOWNLOADS_WIN%\%DEVKIT_NAME%.zip -L https://ziglang.org/deps/%DEVKIT_LONGNAME%.zip || goto :curlfail
+  tar -xf %DOWNLOADS_WIN%\%DEVKIT_NAME%.zip || goto :tarfail
+  move %DEVKIT_LONGNAME% %DEVKIT_NAME%
+)
+
+set CMAKE_VER_TMP=
+if exist %CMAKE_EXE_WIN% (for /F "tokens=3" %%g in ('%CMAKE_EXE_WIN% --version') do (
+  set CMAKE_VER_TMP=%%g
+  goto :checkcmake
+))
+:checkcmake
+if not "%CMAKE_VER_TMP%" == "%CMAKE_VERSION%" (
+  curl -o %DOWNLOADS_WIN%\%CMAKE_NAME%.zip -L https://github.com/Kitware/CMake/releases/download/v%CMAKE_VERSION%/cmake-%CMAKE_VERSION%-windows-x86_64.zip || goto :curlfail
+  cd %ZIGROOTBIN_WIN%
+  tar -xf %DOWNLOADS_WIN%\%CMAKE_NAME%.zip || goto :tarfail
+  move %CMAKE_LONGNAME% %CMAKE_NAME%
+)
+
+set NINJA_VER_TMP=
+if exist %NINJA_EXE_WIN% (for /F "tokens=*" %%g in ('%NINJA_EXE_WIN% --version') do (set NINJA_VER_TMP=%%g))
+if not "%NINJA_VER_TMP%" == "%NINJA_VERSION%" (
+  curl -o %DOWNLOADS_WIN%\ninja-%NINJA_VERSION%-win.zip -L https://github.com/ninja-build/ninja/releases/download/v%NINJA_VERSION%/ninja-win.zip || goto :curlfail
+  cd %ZIGROOTBIN_WIN%
+  tar -xf %DOWNLOADS_WIN%\ninja-%NINJA_VERSION%-win.zip || goto :tarfail
+  cd %ZIGROOT_WIN%
+)
+
+if not exist %ZIG_SRC_WIN%/ (
+  git clone https://github.com/ziglang/zig.git zig-src
+)
+
+cd %ZIG_SRC_WIN%
+git fetch
+git checkout %ZIG_COMMIT%
+git pull
+
+mkdir %ZIG_BUILD_WIN%
+cd %ZIG_BUILD_WIN%
+cmake .. -GNinja -DCMAKE_PREFIX_PATH="%DEVKIT%" -DCMAKE_C_COMPILER="%ZIG_EXE%;cc" -DCMAKE_CXX_COMPILER="%ZIG_EXE%;c++" -DCMAKE_AR="%ZIG_EXE%" -DZIG_AR_WORKAROUND=ON -DZIG_STATIC=ON -DZIG_USE_LLVM_CONFIG=OFF %ZIG_CMAKE_FLAGS% || goto :cmakefail
+ninja install || goto :ninjafail
+
+:curlfail
+:tarfail
+:cmakefail
+:ninjafail
+cd %ZIGROOT_WIN%
